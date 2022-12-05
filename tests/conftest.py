a09fafbfbc13457219ae85297fca085c89f27d4b
irina <neeiraaaa@gmail.com>
2022-12-05 22:24:37 +0300

    finished writing the test

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,8 @@ from selene.support.shared import browser
 @pytest.fixture(scope="function", autouse=True)
 def open_browser():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1366
-    browser.config.window_height = 768
+    browser.config.window_width = 2560
+    browser.config.window_height = 1440
 
     yield
 
--- tests/practice_form_test.py
+++ tests/practice_form_test.py
@@ -1,6 +1,5 @@
 from selene.support.shared import browser
 from selene import have
-from selene import command
 import os
 
 def test_form():
@@ -8,12 +7,47 @@ def test_form():
     browser.element('#firstName').type('Irina')
     browser.element('#lastName').type('Rogova')
     browser.element('#userEmail').type('neeiraaaa@gmail.com')
-    browser.element('#userNumber').type('+79253999999')
+    browser.element('#gender-radio-2').double_click()
+    browser.element('#userNumber').type('79253999999')
     browser.element('#dateOfBirthInput').click()
     browser.element('[class="react-datepicker__month-select"]').click()
     browser.element('[value="9"]').click()
     browser.element('[class="react-datepicker__year-select"]').click()
     browser.element('[value="1997"]').click()
     browser.element('[class="react-datepicker__day react-datepicker__day--025 react-datepicker__day--weekend"]').click()
+
     browser.element('#subjectsInput').set_value('eng').press_enter()
     browser.element('[for="hobbies-checkbox-3"]').click()
+    browser.element('#uploadPicture').set_value(
+        os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'tests/img.png')))
+
+    browser.element('#currentAddress').type('Москва')
+    browser.element('#state').click()
+    browser.element('#react-select-3-input').press_enter()
+    browser.element('#city').click()
+    browser.element('#react-select-4-input').press_enter()
+    browser.element('#submit').click()
+
+    browser.all('.table-responsive td:nth-child(2)').should(have.texts(
+        'Irina Rogova',
+        'neeiraaaa@gmail.com',
+        'Female',
+        '79253999999',
+        '25 October,1997',
+        'English',
+        'Music',
+        'img.png',
+        'Москва',
+        'Haryana Karnal'
+    ))
+
+
+
+
+
+
+
+
+
+
+
